6861277d71f4c4ec00496d40064424653e730985
Kajetan Bancerz <kajetan.bancerz@gmail.com>
2020-07-24 17:06:20 +0200

    Version 0.1.0

---
+++ remote_sqlitedict.py
@@ -0,0 +1,60 @@
+import json
+import os
+
+import rpyc
+from rpyc.utils.server import ThreadedServer
+from sqlitedict import SqliteDict
+
+
+def get_sqlitedict(host, port, db_name, autocommit=False):
+    c = rpyc.connect(host, port)
+    return c.root.get_sqlitedict(db_name, autocommit=autocommit)
+
+
+def get_service(db_root):
+
+    class SQLiteDictService(rpyc.Service):
+
+        DB_ROOT = db_root
+
+        def __init__(self):
+            self._instance = None
+
+        def exposed_get_sqlitedict(self, db_name, **kwargs):
+            db_path = os.path.join(self.DB_ROOT, db_name + '.sqlite')
+            self._instance = SqliteDict(
+                db_path, encode=json.dumps, decode=json.loads, **kwargs)
+            return self._instance
+
+        def on_disconnect(self, conn):
+            if self._instance is not None:
+                self._instance.close()
+
+    return SQLiteDictService
+
+
+def start_server(port, db_root):
+    ThreadedServer(
+        get_service(db_root), port=port,
+        protocol_config={
+            "allow_public_attrs": True,
+            "allow_all_attrs": True
+        }
+    ).start()
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', '-d', default=os.getcwd(),
+                        help='Specify alternative directory '
+                             '[default:current directory]')
+    parser.add_argument('port', action='store',
+                        default=18753, type=int,
+                        nargs='?',
+                        help='Specify alternate port [default: 18753]')
+    args = parser.parse_args()
+
+    print(f'Server starting on port {args.port}...')
+    start_server(args.port, args.directory)
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages  # noqa: H301
+
+NAME = "remote-sqlitedict"
+VERSION = "0.1.0"
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = [
+    "plumbum==1.6.9",
+    "rpyc==4.1.5",
+    "sqlitedict==1.6.0"
+]
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="Remote sqlitedict",
+    author_email="",
+    url="",
+    keywords=["sqlitedict", "rpyc", "remote"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    This package allows for a remote access to SQLite backed dictionary for data persistence   # noqa: E501
+    """
+)
