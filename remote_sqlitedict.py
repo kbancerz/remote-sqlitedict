ac989c0780ba30d377c242ac09e413b2427243a3
Kajetan Bancerz <kajetan.bancerz@gmail.com>
2020-07-27 16:48:37 +0200

    Add wrapper to returned remote sqlitedict

--- remote_sqlitedict.py
+++ remote_sqlitedict.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import rpyc
+from rpyc.core.protocol import PingError
 from rpyc.utils.server import ThreadedServer
 from sqlitedict import SqliteDict
 
@@ -14,14 +15,45 @@ def json_dumps(obj):
 
 
 DEF_PORT = 18753
+PING_TIMEOUT = 1
+
+
+class RemoteSQLiteDict(object):
+    def __init__(self, host, port, db_name, autocommit):
+        self._host = host
+        self._port = port
+        self._db_name = db_name
+        self._autocommit = autocommit
+
+        self._connection = None
+
+    def __enter__(self):
+        make_connection = False
+
+        if self._connection is not None:
+            try:
+                self._connection.ping(timeout=PING_TIMEOUT)
+            except (PingError, EOFError):
+                make_connection = True
+        else:
+            make_connection = True
+
+        if make_connection:
+            self._connection = rpyc.connect(self._host, self._port, config={
+                'allow_public_attrs': True,
+                'allow_all_attrs': True,
+            })
+
+        return self._connection.root.proxy_sqlitedict(
+            self._db_name, autocommit=self._autocommit)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._connection.close()
+        self._connection = None
 
 
 def get_sqlitedict(host, port, db_name, autocommit=False):
-    c = rpyc.connect(host, port, config={
-            'allow_public_attrs': True,
-            'allow_all_attrs': True,
-        })
-    return c.root.get_sqlitedict(db_name, autocommit=autocommit)
+    return RemoteSQLiteDict(host, port, db_name, autocommit=autocommit)
 
 
 def start_server(port, db_root, single_db):
@@ -33,7 +65,7 @@ def start_server(port, db_root, single_db):
         def __init__(self):
             self._instance = None
 
-        def exposed_get_sqlitedict(self, db_name, **kwargs):
+        def exposed_proxy_sqlitedict(self, db_name, **kwargs):
             if single_db:
                 db_path = os.path.join(self.DB_ROOT, '_root.sqlite')
             else:
